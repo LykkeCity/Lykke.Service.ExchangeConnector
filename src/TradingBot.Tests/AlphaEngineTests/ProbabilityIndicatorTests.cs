d6a2f6e0bf747be33d991e26a87dec7a21bbfb7a
Andrey Andreev <AndreevAndrey@ya.ru>
2017-05-24 18:27:57 +0300

    Implementation of Probability Indicator calculations

---
+++ src/TradingBot.Tests/AlphaEngineTests/ProbabilityIndicatorTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingBot.AlphaEngine;
+using Xunit;
+
+namespace TradingBot.Tests.AlphaEngineTests
+{
+    public class ProbabilityIndicatorTests
+    {
+        [Fact]
+        public void CalcFirstDifference_DifferenceInThirdPosition_Return2()
+        {
+            var result = ProbabilityIndicator.CalculateFirstDifference(new byte[] { 0, 0, 1, 0 });
+
+            Assert.Equal(2, result);
+        }
+
+
+        [Fact]
+        public void CalcFirstDifference_NoDifferences_ReturnOne()
+        {
+            var result = ProbabilityIndicator.CalculateFirstDifference(new byte[] { 0, 0, 0, 0 });
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void CalculateCase1()
+        {
+            var prevState = new byte[] { 0, 1, 1, 1};
+            var curState = new byte[] { 1, 1, 1, 1 };
+            var deltas = new decimal[] { 0.00025m, 0.0005m };
+
+            var result = ProbabilityIndicator.Calculate(prevState, curState, deltas);
+
+            Assert.Equal(0.36787944117144233, result);
+        }
+
+        [Fact]
+        public void CalculateCase2()
+        {
+            var prevState = new byte[] { 0, 1, 1, 1 };
+            var curState = new byte[] { 0, 0, 1, 1 };
+            var deltas = new decimal[] { 0.00025m, 0.0005m };
+
+            var result = ProbabilityIndicator.Calculate(prevState, curState, deltas);
+            
+            Assert.Equal(0.63212055882855767, result);
+        }
+
+        [Fact]
+        public void CalculateCase3()
+        {
+            var prevState = new byte[] { 0, 0, 1, 1 };
+            var curState = new byte[] { 0, 0, 0, 1 };
+            var deltas = new decimal[] { 0.00025m, 0.0005m, 0.001m };
+
+            var result = ProbabilityIndicator.Calculate(prevState, curState, deltas);
+
+            Assert.Equal(0.84348235725033427, result);
+        }
+    }
+}
---
+++ src/TradingBot/AlphaEngine/ProbabilityIndicator.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using TradingBot.Infrastructure;
+
+namespace TradingBot.AlphaEngine
+{
+    /// <summary>
+    /// Calculated as in paper "Multi-scale Representation of High Frequency Market
+    /// Liquidity", p. 10
+    /// </summary>
+    public class ProbabilityIndicator
+    {
+        private static ILogger Logger = Logging.CreateLogger<ProbabilityIndicator>();
+
+        public static int CalculateFirstDifference(byte[] state)
+        {
+            int i;
+            for (i = 1; i < state.Length && state[i] == state[0]; i++)
+            {
+            }
+
+            int firstDifference = i < state.Length ? i : 1;
+
+            return firstDifference;
+        }
+
+        public static double Calculate(byte[] previousState, 
+            byte[] currentState,
+            decimal[] deltas)
+        {
+            double result = 0;
+
+            int firstDifference = CalculateFirstDifference(previousState);
+
+            if (firstDifference == 1)
+            {
+                var exp = Math.Exp(decimal.ToDouble(-(deltas[1] - deltas[0]) / deltas[0]));
+                if (previousState[0] != currentState[0])
+                {
+                    result = exp;
+                }
+                else if (previousState[1] != currentState[1])
+                {
+                    result = 1 - exp;
+                }
+                else
+                {
+                    Logger.LogError("Unexpected Probabilty Indicator state");
+                }
+            }
+            else if (firstDifference > 1)
+            {
+                double numerator = 1;
+                for (int i = 1; i <= firstDifference; i++)
+                {
+                    numerator *= Math.Exp(decimal.ToDouble(- (deltas[i] - deltas[i-1]) / deltas[i-1]));
+                }
+                
+                double sum = 0;
+                for (int i = 1; i < firstDifference; i++)
+                {
+                    double firstVal = Math.Exp(decimal.ToDouble(-(deltas[i] - deltas[i - 1]) / deltas[i - 1]));
+                    double mult = 1;
+
+                    for (int j = i + 1; j <= firstDifference; j++)
+                    {
+                        mult *= Math.Exp(decimal.ToDouble(-(deltas[j] - deltas[j - 1]) / deltas[j - 1]));
+                    }
+                    sum += firstVal * mult;
+                }
+                
+                double denominator = 1 - sum;
+
+                if (currentState[0] != previousState[0])
+                {
+                    result = numerator / denominator;
+                }
+                else if (currentState[firstDifference] != previousState[firstDifference])
+                {
+                    result = 1 - numerator / denominator;
+                }
+            }
+            else
+            {
+                Logger.LogError($"Unexpected {nameof(firstDifference)} value {firstDifference}");
+            }
+
+            return result;
+        }
+    }
+}
