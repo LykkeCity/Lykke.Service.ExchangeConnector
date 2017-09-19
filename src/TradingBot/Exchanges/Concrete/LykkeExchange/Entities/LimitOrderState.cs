c128b94f2134d720642d164311e813d6172195b8
Andrey Andreev <AndreevAndrey@ya.ru>
2017-09-19 03:41:04 +0300

    Nullable dates in LimitOrderState

--- src/TradingBot/Exchanges/Concrete/LykkeExchange/Entities/LimitOrderState.cs
+++ src/TradingBot/Exchanges/Concrete/LykkeExchange/Entities/LimitOrderState.cs
@@ -18,11 +18,11 @@ namespace TradingBot.Exchanges.Concrete.LykkeExchange.Entities
         
         public decimal RemainingVolume { get; set; }
         
-        public DateTime LastMatchTime { get; set; }
+        public DateTime? LastMatchTime { get; set; }
         
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         
-        public DateTime Registered { get; set; }
+        public DateTime? Registered { get; set; }
     }
 
     public enum LimitOrderStatus
