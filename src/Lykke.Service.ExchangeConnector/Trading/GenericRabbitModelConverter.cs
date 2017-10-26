2660d7b552b4029fcc0f7d64ef4843f391bde91d
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2017-10-26 13:34:42 +0200

    Change date time format for the default RabbitMq publisher. Use lykke's currency in the published order book

--- src/Lykke.Service.ExchangeConnector/Trading/GenericRabbitModelConverter.cs
+++ src/Lykke.Service.ExchangeConnector/Trading/GenericRabbitModelConverter.cs
@@ -6,23 +6,28 @@ using Newtonsoft.Json.Serialization;
 
 namespace TradingBot.Trading
 {
-    public class GenericRabbitModelConverter<T> : IRabbitMqSerializer<T>, IMessageDeserializer<T>
+    internal sealed class GenericRabbitModelConverter<T> : IRabbitMqSerializer<T>, IMessageDeserializer<T>
     {
+        private const string Iso8601DateFormat = @"yyyy-MM-ddTHH:mm:ss.fffzzz";
+        private readonly JsonSerializerSettings _serializeSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatString = Iso8601DateFormat
+        };
+
+        private readonly JsonSerializerSettings _deserializeSettings = new JsonSerializerSettings
+        {
+            DateFormatString = Iso8601DateFormat
+        };
+
         public byte[] Serialize(T model)
         {
-            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model, new JsonSerializerSettings()
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                DateFormatString = "yyyy-MM-ddTHH:mm:ss.fff"
-            }));
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model, _serializeSettings));
         }
 
         public T Deserialize(byte[] data)
         {
-            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data), new JsonSerializerSettings()
-            {
-                DateFormatString = "yyyy-MM-ddTHH:mm:ss.fff"
-            });
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data), _deserializeSettings);
         }
     }
 }
