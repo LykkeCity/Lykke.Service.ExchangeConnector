d6a5f21495e16316dd4852e2660b1c5f4f4920ba
Petr Giloian <giloyanp@gmail.com>
2017-11-09 16:39:19 +0700

    Fixed WampSubscriber reconnection procedure.

--- src/Lykke.Service.ExchangeConnector/Infrastructure/Wamp/WampSubscriber.cs
+++ src/Lykke.Service.ExchangeConnector/Infrastructure/Wamp/WampSubscriber.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using Autofac;
 using Common;
 using Common.Log;
-using WampSharp.V2;
-using WampSharp.V2.Client;
-using WampSharp.V2.Fluent;
 using WampSharp.Binding;
 using WampSharp.Core.Listener;
-using Newtonsoft.Json.Linq;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+using WampSharp.V2.Realm;
 using WampSharp.WebSockets;
+using Newtonsoft.Json.Linq;
 using TradingBot.Infrastructure.Configuration;
 
 namespace TradingBot.Infrastructure.Wamp
@@ -27,6 +27,8 @@ namespace TradingBot.Infrastructure.Wamp
         private CancellationTokenSource cancelToken = null;
         private bool isDisposed = false;
         private Action<T> handlers;
+        private static readonly object _sync = new object();
+        private bool isConnected = false;
 
         public WampSubscriber(WampSubscriberSettings settings)
         {
@@ -73,17 +75,82 @@ namespace TradingBot.Infrastructure.Wamp
             return this;
         }
 
-        public void Dispose()
+        private void OpenConnectionAndSub()
         {
-            this.Dispose(true);
+            // 1. Connect
+            // 
+
+            var binding = new JTokenJsonBinding();
+            Func<IControlledWampConnection<JToken>> connectionFactory =
+                    () => new ControlledTextWebSocketConnection<JToken>(new Uri(this.settings.Address), binding);
+
+            this.channel = new WampChannelFactory().CreateChannel(this.settings.Realm, connectionFactory, binding);
+
+            var monitor = this.channel.RealmProxy.Monitor;
+            monitor.ConnectionBroken += OnConnectionBroken;
+            monitor.ConnectionError += OnConnectionError;
+
+            while (!monitor.IsConnected)
+            {
+                if (this.cancelToken.IsCancellationRequested)
+                {
+                    this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Finishing connecting to server.");
+                    return;
+                }
+
+                try
+                {
+                    this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Trying to connect to server: {this.settings.Address} realm: {this.settings.Realm}");
+                    this.channel.Open().Wait();
+                }
+                catch (Exception ex)
+                {
+                    this.log?.WriteErrorAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, ex);
+                    this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, "Retrying to connect in 5 sec...");
+                    Thread.Sleep(5000);
+                }
+            }
+            this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Connected to server {this.settings.Address}");
+
+            // 2. Subscribe to topic
+            //
+            foreach (var topic in this.settings.Topics)
+            {
+                var subj = this.channel.RealmProxy.Services.GetSubject<T>(topic);
+                this.subscriptions.Add(subj.Subscribe(this.OnNext, this.OnError, this.OnCompleted));
+                this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Subscribed to topic '${topic}'.");
+            }
+
+            this.isConnected = true;
         }
 
-        protected virtual void Dispose(bool disposing)
+        private void OnConnectionError(object sender, WampConnectionErrorEventArgs e)
         {
-            if (!this.isDisposed)
+            this.log?.WriteErrorAsync(nameof(WampSubscriber<T>), nameof(OnConnectionError), string.Empty, e?.Exception);
+            Reconnect();
+        }
+
+        private void OnConnectionBroken(object sender, WampSessionCloseEventArgs e)
+        {
+            this.log?.WriteWarningAsync(nameof(WampSubscriber<T>), nameof(OnConnectionBroken), string.Empty,
+                $"Connection is broken. Reason: {e?.Reason}. Message: {e?.Details?.Message}");
+            Reconnect();
+        }
+
+        private void Reconnect()
+        {
+            if (this.isConnected)
             {
-                this.Stop();
-                this.isDisposed = true;
+                lock (_sync)
+                {
+                    if (this.isConnected)
+                    {
+                        this.isConnected = false;
+
+                        CloseConnection();
+                        OpenConnectionAndSub();
+                    }
+                }
             }
         }
 
@@ -105,15 +172,25 @@ namespace TradingBot.Infrastructure.Wamp
         protected virtual void OnError(Exception ex)
         {
             this.log?.WriteErrorAsync(nameof(WampSubscriber<T>), nameof(OnError), string.Empty, ex);
-            CloseConnection();
-            OpenConnectionAndSub();
         }
 
         protected virtual void OnCompleted()
         {
-            this.log?.WriteWarningAsync(nameof(WampSubscriber<T>), nameof(OnError), string.Empty, "Subscription is completed.");
-            CloseConnection();
-            OpenConnectionAndSub();
+            this.log?.WriteWarningAsync(nameof(WampSubscriber<T>), nameof(OnCompleted), string.Empty, "Subscription is completed.");
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                this.Stop();
+                this.isDisposed = true;
+            }
         }
 
         private void ValidateInstance()
@@ -137,55 +214,14 @@ namespace TradingBot.Infrastructure.Wamp
             this.subscriptions.ForEach(i => i.Dispose());
             this.subscriptions.Clear();
 
-            if (this.channel != null && this.channel.RealmProxy.Monitor.IsConnected)
+            if (this.channel != null)
             {
+                var monitor = this.channel.RealmProxy.Monitor;
+                monitor.ConnectionBroken -= OnConnectionBroken;
+                monitor.ConnectionError -= OnConnectionError;
+
                 this.channel.Close();
                 this.channel = null;
-            }
-        }
-
-        private void OpenConnectionAndSub()
-        {
-            // 1. Connect
-            //
-            var binding = new JTokenJsonBinding();
-            Func<IControlledWampConnection<JToken>> connectionFactory =
-                    () => new ControlledTextWebSocketConnection<JToken>(new Uri(this.settings.Address), binding);
-
-            this.channel = new WampChannelFactory().CreateChannel(this.settings.Realm, connectionFactory, binding);
-
-            while (!this.channel.RealmProxy.Monitor.IsConnected)
-            {
-                try
-                {
-                    this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Trying to connect to server: {this.settings.Address} realm: {this.settings.Realm}");
-                    this.channel.Open().Wait();
-                }
-                catch (Exception ex)
-                {
-                    this.log?.WriteErrorAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, ex);
-                    if (ex is AggregateException)
-                    {
-                        var ae = ex as AggregateException;
-
-                        foreach (var e in ae.Flatten().InnerExceptions)
-                        {
-                            this.log?.WriteErrorAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, e);
-                        }
-                    }
-                    this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, "Retrying to connect in 5 sec...");
-                    Thread.Sleep(5000);
-                }
-            }
-            this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Connected to server {this.settings.Address}");
-
-            // 2. Subscribe to topic
-            //
-            foreach (var topic in this.settings.Topics)
-            {
-                var subj = this.channel.RealmProxy.Services.GetSubject<T>(topic);
-                this.subscriptions.Add(subj.Subscribe(this.OnNext, this.OnError, this.OnCompleted));
-                this.log?.WriteInfoAsync(nameof(WampSubscriber<T>), nameof(Start), string.Empty, $"Subscribed to topic '${topic}'.");
             }
         }
     }
