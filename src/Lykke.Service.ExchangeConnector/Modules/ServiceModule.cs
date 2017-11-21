53293df21a3082f75bbd59133b76881b632c6010
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2017-11-21 15:56:26 +0100

    Fix jfd the order book feed name

--- src/Lykke.Service.ExchangeConnector/Modules/ServiceModule.cs
+++ src/Lykke.Service.ExchangeConnector/Modules/ServiceModule.cs
@@ -66,7 +66,7 @@ namespace TradingBot.Modules
                 .SingleInstance();
 
             builder.RegisterType<JfdOrderBooksHarvester>()
-                .WithParameter("exchangeName", GdaxExchange.Name)
+                .WithParameter("exchangeName", JfdExchange.Name)
                 .SingleInstance();
 
             builder.RegisterType<JfdTradeSessionConnector>()
