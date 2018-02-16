30eb36ebe2cdccc447ab507a7a22408e3c364252
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-02-16 16:30:19 +0200

    remove reference to appsettings.json from Startup.cs

--- src/Lykke.Service.ExchangeConnector/Startup.cs
+++ src/Lykke.Service.ExchangeConnector/Startup.cs
@@ -40,7 +40,6 @@ namespace TradingBot
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
